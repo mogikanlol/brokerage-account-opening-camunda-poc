62d0fd64db23edc2af80bc4a6f5c4803ff4a7d5f
Dmitry Nikolaev <dmitrii_nikolaev1@epam.com>
2019-09-01 18:56:27 +0300

    Add a process

---
+++ camunda-demo-app/src/main/java/com/example/bpm/deposit/configuration/CamundaProcessApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.bpm.deposit.configuration;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableProcessApplication
+public class CamundaProcessApplicationConfiguration {
+}
